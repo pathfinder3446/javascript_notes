5caa0ddc116e1ac3f9cc47c67743a055cb6748ae
Arif <kanebrian0634@gmail.com>
2022-05-10 20:44:19 +0300

    session 04 was added

---
+++ s04/loops.js
@@ -0,0 +1,13 @@
+console.log(`*******LOOPS******`);
+
+for(let i=1 ; i<=10 ; i++){
+    const randomsayi = Math.floor(Math.random() *100 + 1) ;
+    console.log(randomsayi)
+}
+
+// Math.random() 0 ile 1 arasında rastgele sayı üretir
+// Math.floor() sayıyı tabana yuvarlar
+// Math.ceil yukarı yuvarlar
+// Math.round() değerine göre yuvarlar
+
+console.log(`Program Bitti`);
