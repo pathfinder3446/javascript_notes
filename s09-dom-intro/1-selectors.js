ef7daf521e39506d99b7e6c6bdf420b1b1824c1b
Arif <kanebrian0634@gmail.com>
2022-05-28 13:22:39 +0300

    updated

--- s09-dom-intro/1-selectors.js
+++ s09-dom-intro/1-selectors.js
@@ -40,9 +40,34 @@ img[0].style.transform = 'rotate(-5deg)';
 //*======================================================
 //*                   GETELEMENTSBYCLASSNAME()
 //* ======================================================
-
 //* EXAMPLE-4
 //* ---------------
-
 const par = document.getElementsByClassName('par');
-par[0].textContent = 'DOM Manipülasyonu';
+// par[0].textContent = 'DOM Manipülasyonu';
+// par[0].innerText = 'DOM Manipülasyonu';
+par[0].innerHTML += ' <a href="https://www.clarusway.com">Clarusway</a>';
+
+//* ========================================
+//*              QUERYSELECTOR()
+//* ========================================
+document.querySelector('title').textContent = 'JS09-DOM ❤️';
+
+const myPars = document.querySelectorAll('p');
+console.log(myPars); //? NodeList
+myPars.forEach((p) => console.log(p.innerText));
+
+const myPars1 = document.getElementsByClassName('par');
+console.log(myPars); //? HTML Collection
+
+//! HTML Collection'dan Array' e çevirme yöntemleri
+//? 1.Array.from()
+const myParsArray = Array.from(myPars1); //? Array.from ile diziye çevirilir.
+console.log(myParsArray);
+myParsArray.forEach((p) => console.log(p.innerText));
+
+//? 2.Spread/rest
+[...myPars1].forEach((p) => console.log(p.innerText));
+
+//! querySelector ile CSS vari seçim yapmak mümkündür.
+//! CSS Selector'lerin hepsini kullanmak mümkündr.
+console.log(document.querySelector('section p a'));
