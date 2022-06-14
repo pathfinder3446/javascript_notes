55ba2a15402ce90613d3e8438d8418ddff554d95
Arif <kanebrian0634@gmail.com>
2022-06-14 23:13:00 +0300

    updated

--- s15_async1/3-fetchAPI.js
+++ s15_async1/3-fetchAPI.js
@@ -10,3 +10,26 @@
 
 //? fetch() fonksiyonu veri getirmek istediginiz kaynagin yolunu gosteren zorunlu
 //? bir parametre almaktadir ve bu istegin cevabini gosteren bir Promise dondurmektedir.
+
+fetch('https://api.github.com/users')
+  .then((res) => {
+    //! error handling
+    if (!res.ok) {
+      throw new Error(`Something went wrong: ${res.status}`);
+    }
+    return res.json();
+  })
+  .then((data) => updateDom(data))
+  .catch((err) => console.log(err));
+
+const updateDom = (data) => {
+  const userDiv = document.querySelector('.users');
+
+  data.forEach((user) => {
+    const { login, avatar_url, html_url } = user;
+    userDiv.innerHTML += `
+    <h2 class="text-warning">NAME:${login}</h2>
+    <img src=${avatar_url} width="50%" alt="" />;
+  `;
+  });
+};
