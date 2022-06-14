39a0c9f675a424d583bdd9158a11a68cdb482c7a
Arif <kanebrian0634@gmail.com>
2022-06-14 20:36:17 +0300

    initialized

---
+++ s15_async1/1-timeout_Interval.js
@@ -0,0 +1,32 @@
+//*=================================================================
+//*              1- Senkron-Asenkron Programlama
+//* ================================================================
+
+//! Javascript, bir single-threaded ve Asenkron Programlama dilidir.
+
+//? Asenkron Programlama
+//? ---------------------------------------------------------------
+//? Asenkron Programlama, bir alt gorevin uygulamanin asil thread'inden
+//? bagimsiz olarak arka planda calistirilmasina izin veren paralel programlama
+//? teknigidir. Bu alt gorev tamamlandiginda (basriyla veya basarisizlikla)
+//? asil thread bu konuda bilgilendirilir. Asenkron programlama, uygulamalarin
+//? performansininin artirilmasina ve daha iyi kullanici deneyimine katki saglamaktadir.
+
+//? Ozellikle bir API'den veya Veritabanindan veri cekme, Giris/Cikis islemleri,
+//? Dosya Okuma/Yazma islemleri gibi zaman tuketen kodlarda Asyn Programlama
+//? kullanilmasi cok onemlidir.
+
+//* Senkron
+//* ---------------------------------------------------------------
+const bekle = (ms) => {
+    const start = new Date().getTime();
+    2000;
+    while (new Date().getTime() < start + ms){}
+};
+
+
+
+console.log('HELLO');
+// alert(`ITF`) //! blocking code
+bekle(3000);
+console.log('FS11');
