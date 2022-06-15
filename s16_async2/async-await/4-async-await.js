88e6eefcaeb0ff5b7aefb0630b27ea42349bfa11
Arif <kanebrian0634@gmail.com>
2022-06-15 23:19:16 +0300

    ...

--- s16_async2/async-await/4-async-await.js
+++ s16_async2/async-await/4-async-await.js
@@ -17,3 +17,37 @@
 //* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
+
+let error = false;
+const getUsers = async function () {
+  try {
+    const res = await fetch('https://api.github.com/user');
+    if (!res.ok) {
+      error = true;
+      throw new Error(`Something went wrong:${res.status}`);
+    }
+    const data = await res.json();
+    updateDom(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getUsers();
+
+const updateDom = (data) => {
+  const userDiv = document.querySelector('.users');
+  if (error) {
+    userDiv.innerHTML = `<h1 class="text-danger">Data can not be fetched</h1>`;
+  } else {
+    data.forEach((user) => {
+      //!destr
+      const { login, avatar_url, html_url } = user;
+      userDiv.innerHTML += `
+    <h2 class="text-warning">NAME:${login}</h2>
+    <img src=${avatar_url} width="50%" alt="" />
+    <h3>HTML_URL:${html_url}</h3>
+  `;
+    });
+  }
+};
