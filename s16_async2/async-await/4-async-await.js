748b47993518ef47731a8c9cd23178c00b5f53ad
Arif <kanebrian0634@gmail.com>
2022-06-15 20:17:19 +0300

    async2 session added

---
+++ s16_async2/async-await/4-async-await.js
@@ -0,0 +1,19 @@
+//*===============================================================
+//*                   4- ASYNC-AWAIT
+//*===============================================================
+//? Async-Await ECMAScript 2017 ile Javascript diline eklenmistir.
+//? Aslinda Promise yapisinin syntax olarak basitlestirilmis halidir.
+//? Bu baglamda sentetik seker benzetmesi yapilabilir.
+
+//* Bir fonskiyonu asyn hale getirmek icin fonksiyon keyword'nun onune
+//* asyn keyword'u eklenir.
+
+//* Bir async fonksiyon icerisinde await keyword'u ile yapilan istegin cevabinin
+//* beklenmesi saglanir.
+
+//* Aslinda dizilis olarak senkron mantiga benzeyen kod yazarak Asenkron
+//* kod yazmayı mumkun kilar.
+
+//* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
+//* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
+//* degerlerinin dondurulmesine ile kodun calismasi devam eder.
